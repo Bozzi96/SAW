a694ce171a3d7ad0f26ed24045877da228010679
nicsalv <40719267+nicsalv@users.noreply.github.com>
2018-07-02 15:08:41 +0200

    database folder
    Questa cartella contiene il file di connessione da includere in ogni script che necessita di accedere al database.
    Ognuno poi deve immettere le proprie credenziali.

---
+++ db/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+    require 'mysql_credentials.php';
+
+    // Begin connection
+    $conn = new mysqli($mysql_server, $mysql_user, $mysql_pass, $mysql_db);
+
+    // Check connection
+    if ($conn -> connect_error) {
+        die("Connessione al database fallita: " . $conn -> connect_error);
+    } else {
+        echo "Connessione al database avvenuta";
+    }
+    
+?>
