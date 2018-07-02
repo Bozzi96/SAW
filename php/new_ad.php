ac4ec4d077838c8a0fcebb9abb11568fe3f06e8d
nicsalv <40719267+nicsalv@users.noreply.github.com>
2018-07-02 15:50:04 +0200

    salvataggio nuovo annuncio su db
    script per inserire un nuovo annuncio (salvataggio dei dati nel db).

---
+++ php/new_ad.php
@@ -0,0 +1,34 @@
+<?php
+    
+    # Sanitizzazione dell'input
+    function sanitize_input($conn, $str) {
+        $str = trim($str);
+        $str = $conn -> real_escape_string($str);
+        return $str;
+    }
+
+    # Connessione al db
+    require '../db/connection.php';
+
+    # Raccolta delle info dell'annuncio
+    $v_name = sanitize_input($conn, $_POST['v_name']);
+    $console = sanitize_input($conn, $_POST['console']);
+    $loan_length = $_POST['loan_length'];
+    $price = $_POST['price'];
+
+    # Preparazione dello statement per il database
+    $stmt = $conn -> prepare("INSERT INTO annunci (email, nome_videogioco, console, prezzo, durata, stato) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt -> bind_param("sssiis", $email, $v_name, $console, $loan_length, $price, $status);
+
+    # Settaggio dei parametri ed esecuzione della query
+    $email = "address@mail.com";
+    $status = "Disponibile";
+    $stmt -> execute();
+
+    echo "Annuncio inserito correttamente.";
+
+    # Chiusura della connessione
+    $stmt -> close();
+    $conn -> close();
+
+?>
