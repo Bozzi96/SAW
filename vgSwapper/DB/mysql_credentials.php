acb3b1ad3209f5f4ce38db9895847f2321101bcb
Your Name <you@example.com>
2018-07-09 17:40:05 +0200

    rimozione bootstrap

--- vgSwapper/DB/mysql_credentials.php
+++
@@ -1,8 +0,0 @@
-<?php
-        //inserisci le tue credenziali per la connessione!!
-        $username='root';
-        $database='videogameSwap_DB';//nome DB
-        $psw='root';
-        $host='localhost';//server
-   
-?>
