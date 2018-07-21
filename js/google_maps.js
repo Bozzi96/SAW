0628718e4284357b53eec69439d9a008ee953467
Ilbotre <40883366+Ilbotre@users.noreply.github.com>
2018-07-21 19:18:32 +0200

    geolocalizzazione_fixata

--- js/google_maps.js
+++ js/google_maps.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Regular map. il marker viene inserito nella mappa data una coppia (latitudine, longitudine)
+// Regular map. il marker viene inserito nella posizione attuale attraverso la geolocalizzazione
 function regular_map(latd,lngt) {
     var var_location = new google.maps.LatLng(latd, lngt); //definisce un punto nel globo!
 
@@ -8,22 +8,55 @@ function regular_map(latd,lngt) {
         center: var_location,
         zoom: 14
     };
-
-    var var_map = new google.maps.Map(document.getElementById("map-container"), //inizializza la mappa nel dom
+    var map = new google.maps.Map(document.getElementById("map-container"), //inizializza la mappa nel dom
         var_mapoptions);
 
-    var var_marker = new google.maps.Marker({ //setta il marker
-        position: var_location,
-        map: var_map,
-    });
+    var infoWindow = new google.maps.InfoWindow;
+
+    //HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Siete qui.');
+            infoWindow.open(map);
+            //setta il marker
+            var var_marker = new google.maps.Marker({ 
+            position: pos,
+            map: map,
+            title: "SEI QUI!"
+            });
+
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
 // Initialize maps
-google.maps.event.addDomListener(window, 'load', getPosition);
+google.maps.event.addDomListener(window, 'load', getInformation);
 
 
 /**
  * questa funzione recupera città e cap dal server attraverso una chiamata AJAX
- *  -->obsoleta se si usa la geolocalizzazione!
+ *  (-->obsoleta se si usa la geolocalizzazione!)
+ * in ogni caso viene usata per recuperare dal server la posizione dell'utente inserita in fase si SignUp
  */
 function getInformation() {
     var xhttp;
@@ -44,7 +77,7 @@ function getInformation() {
             getLatLngt(cityName, capValue);
         }           
     }
-    xhttp.open("GET","../php/get_informations.php", true);//!!
+    xhttp.open("GET","../php/get_informations.php", true);
     xhttp.send();
 }
 
@@ -54,7 +87,7 @@ function getInformation() {
  * @param {*} cityName 
  * @param {*} capValue 
  */
-function getPosition(cityName, capValue ) {
+function getLatLngt(cityName, capValue) {
     var xhttp;
     var myObj, myJSON, latd, lngt;
     if (window.XMLHttpRequest) {
@@ -67,13 +100,16 @@ function getPosition(cityName, capValue ) {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             myObj = JSON.parse(this.responseText);//parsa i dati arrivati in formato JSON
-            //alert("Geolocalizzazione con raggio d'errore di: "+myObj.accuracy+" metri.");
-            latd = myObj.location.lat;
-            lngt = myObj.location.lng;
-            regular_map(latd, lngt);
-            
+            //dobbiamo aquisire latitudine e longitudine
+            if (myObj.status == "OK") {
+                latd = myObj.results[0].geometry.location.lat;
+                lngt = myObj.results[0].geometry.location.lng;
+                regular_map(latd, lngt);
+            } else {
+                alert("error occuring in decoding position");
+            }
         }
     }
-    xhttp.open("POST", "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyB58OmlW7yIXR--pf3dE5djLqEVI0UqujY", true);//!!
+    xhttp.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?address=" + cityName + "+" + capValue + "&key=AIzaSyB58OmlW7yIXR--pf3dE5djLqEVI0UqujY", true);//!!
     xhttp.send();
 }
