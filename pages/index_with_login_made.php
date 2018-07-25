64fce632f9a464b11a232c74862703039f745aa8
Your Name <you@example.com>
2018-07-25 16:52:45 +0200

    fix homepage logged title ahahahahaha

--- pages/index_with_login_made.php
+++ pages/index_with_login_made.php
@@ -36,7 +36,7 @@
     <!--essenziale per strutture responsive-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Homepage</title>
+    <title>vgSWAP</title>
   </head>
 
   <body>
