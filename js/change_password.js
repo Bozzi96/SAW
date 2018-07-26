0676c37630bcd2900250a539b61e1aaf32e3bd8d
nicsalv <you@example.com>
2018-07-26 12:13:03 +0200

    Beautified js code #2

--- js/change_password.js
+++ js/change_password.js
@@ -1,26 +1,26 @@
 "use strict";
 
 /**
-* Funzione stringCompare
-* @param {string} str1 prima stringa
-* @param {string} str2 seconda stringa
-* @returns 0 se le due stringhe sono uguali, 1 se str1 > str2 (somma dei caratteri in codifica ASCII), -1 altrimenti  
-*/
+ * Funzione stringCompare
+ * @param {string} str1 prima stringa
+ * @param {string} str2 seconda stringa
+ * @returns 0 se le due stringhe sono uguali, 1 se str1 > str2 (somma dei caratteri in codifica ASCII), -1 altrimenti  
+ */
 function strcmp(str1, str2) {
     return ((str1 == str2) ? 0 : ((str1 > str2) ? 1 : -1));
 };
 
 
 /**
-*  Funzione che verifica la correttezza delle password inserite.
-*  La funzione va chiamata sia oninput che onchange, altrimenti cancellando il campo "Ripeti password" il pulsante di submit rimane attivo
-*/
+ *  Funzione che verifica la correttezza delle password inserite.
+ *  La funzione va chiamata sia oninput che onchange, altrimenti cancellando il campo "Ripeti password" il pulsante di submit rimane attivo
+ */
 function verifypsw() {
     //Salvataggio parametri
     var oldpsw = document.getElementById("oldPassword").value;
     var psw1 = document.getElementById("newPassword").value;
     var psw2 = document.getElementById("repeatPassword").value;
-    
+
     //Comparazione delle due password
     var compare = strcmp(psw1, psw2);
     if (document.getElementById("repeatPassword").value != "" && document.getElementById("newPassword").value != "") {
@@ -39,7 +39,7 @@ function verifypsw() {
         document.getElementById("afterpsw").innerHTML = "";
         document.getElementById("submitButton").setAttribute("disabled", "true");
     }
-    
+
     //Comparazione tra la password vecchia e quella nuova
     var compareOld = strcmp(oldpsw, psw1);
     if (compareOld == 0 && document.getElementById("oldPassword").value != "") {
@@ -48,5 +48,5 @@ function verifypsw() {
         document.getElementById("afterpsw").innerHTML = color;
         document.getElementById("submitButton").setAttribute("disabled", "true");
     }
-    
+
 };
